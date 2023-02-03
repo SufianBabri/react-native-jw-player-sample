ff15c266e32f47e9b7042dc6b223216f141c4e9d
Sufian Babri <sufian.babri@gmail.com>
2023-02-03 20:26:01 +0500

    Add JW Media Player

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# JW Media Player React Native Sample
+
+This is a sample app with JW Media Player + Chromecast integration in the React Native app.
+
+## Instructions
+
+To run the app, please create a *.env* file at the root of the project with your license key.
+
+> **Note:** Please refer to the *.env.example* for the exact keys.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import { JWP_LICENSE_ANDROID, JWP_LICENSE_IOS } from "react-native-dotenv";
+import JWPlayer from "react-native-jw-media-player";
 
 export default function App() {
+	const playlist = [
+		{
+			mediaId: "1",
+			file: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+			autostart: true
+		},
+		{
+			mediaId: "2",
+			file: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+			autostart: true
+		}
+	];
+
 	return (
-		<SafeAreaView>
-			<Text style={{ color: "orange" }}>Hello, World!</Text>
+		<SafeAreaView style={styles.container}>
+			<JWPlayer
+				style={styles.player}
+				config={{
+					license:
+						Platform.OS === "android" ? JWP_LICENSE_ANDROID : JWP_LICENSE_IOS,
+					backgroundAudioEnabled: true,
+					autostart: true,
+					repeat: false,
+					playlist
+				}}
+			/>
 		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: { flex: 1 },
+	player: { flex: 1 }
+});
